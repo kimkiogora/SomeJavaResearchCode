5fa8509107993a288818a34224a618b3f1f0e043
Kim Kiogora <kimkiogora@gmail.com>
2016-06-08 09:28:49 +0300

    Create TestMemoryMap.java

---
+++ src/TestMemoryMap.java
@@ -0,0 +1,14 @@
+/**
+ * Test memory map
+*/
+class TestMemoryMap{
+    public static void main(String...args){
+      try {
+        String cache_data = MemoryMap.getMemoryContents(logging, props.getQueueSaveFile());
+        System.out.println(cache_data);
+      } catch(IOException e){
+            logging.error(Utilities.getLogPreString()
+                    + "Unable to load saved queues...");
+      }
+    }
+}
